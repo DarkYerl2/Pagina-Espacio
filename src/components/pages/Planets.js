855e487f62b127a901ddb24e6a9284b39ecb0c6a
Daniel <fazeyerl@gmail.com>
2021-09-04 18:53:25 -0500

    Terminado

--- src/components/pages/Planets.js
+++ src/components/pages/Planets.js
@@ -29,37 +29,37 @@ function Planets() {
           <ul className='cards__items'>
             <CardItem
               src='images/tierra.jpg'
-              text=''
+              text='La tierra es el único planeta que alberga vida en todo el Sistema Solar.'
               label='Tierra'
               path='/planets'
             />
             <CardItem
               src='images/marte.jpg'
-              text=''
+              text='Marte es un planeta telúrico con una atmósfera delgada de dióxido de carbono.'
               label='Marte'
               path='/planets'
             />
             <CardItem
               src='images/jupiter.jpg'
-              text=''
+              text='Jupiter es un gigante gaseoso que forma parte de los denominados planetas exteriores'
               label='Jupiter'
               path='/planets'
             />
             <CardItem
               src='images/saturno.jpg'
-              text=''
+              text='Saturno es el segundo en tamaño y masa después de Júpiter y el único con un sistema de anillos visible desde la Tierra'
               label='Saturno'
               path='/planets'
             />
             <CardItem
               src='images/urano.jpg'
-              text=''
+              text=' Se llama así en honor de la divinidad griega del cielo Urano, el padre de Crono y el abuelo de Zeus.'
               label='Urano'
               path='/planets'
             />
             <CardItem
               src='images/neptuno.jpg'
-              text=''
+              text='Es uno de los gigantes helados, y es el primero que fue descubierto gracias a predicciones matemáticas. '
               label='Neptuno'
               path='/planets'
             />
