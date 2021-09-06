b39862e65a5bb882e8010e2c9e3096d71fa2bc83
Daniel <fazeyerl@gmail.com>
2021-09-06 16:23:34 -0500

    about-terminado

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,9 +1,28 @@
 import React from "react";
-import '../../App.css';
+import '../../App.css'
 
+import ReactPlayer from 'react-player'
 
-export default function About() {
-  return <h1 className='about'>
-  ¿Que somos?
-  </h1>;
-}
+  
+  export default function About() {
+    return (
+        
+       
+  
+      <div className='container-about'>
+        <div className='about-item'>
+        <h5>Mira este video para saber mas del universo</h5>
+        <ReactPlayer 
+        url='https://youtu.be/9vTC8co9YAA' 
+        classname='video'
+        controls
+        playing
+        volume='0.8'
+        
+        />
+        </div>  
+      </div>
+    );
+  }
+  
+
