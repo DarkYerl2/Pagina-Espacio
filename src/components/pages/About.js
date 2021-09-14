11de4c09bc9b3cd6335cb67dde5fcd1a8c33d7b2
Daniel <fazeyerl@gmail.com>
2021-09-13 20:54:19 -0500

    full-responsive

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,28 +1,23 @@
 import React from "react";
-import '../../App.css'
+import "../../App.css";
 
-import ReactPlayer from 'react-player'
+import ReactPlayer from "react-player";
 
-  
-  export default function About() {
-    return (
-        
-       
-  
-      <div className='container-about'>
-        <div className='about-item'>
+export default function About() {
+  return (
+    <div className="container-about">
+      <div className="about-item">
         <h5>Mira este video para saber mas del universo</h5>
-        <ReactPlayer 
-        url='https://youtu.be/9vTC8co9YAA' 
-        classname='video'
-        controls
-        playing
-        volume='0.8'
-        
+        <ReactPlayer
+          url="https://youtu.be/9vTC8co9YAA"
+          width="100%"
+          height="100%"
+          classname="video"
+          controls
+          playing
+          volume="0.8"
         />
-        </div>  
       </div>
-    );
-  }
-  
-
+    </div>
+  );
+}
